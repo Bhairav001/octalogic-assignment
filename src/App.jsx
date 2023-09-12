b7c2c0bfde28d55175ecbe6001b47ec1803d1a91
Bhiaravnath Gotam <bhairavgotam066@gmail.com>
2023-09-13 00:11:30 +0530

    added tailwind css update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
        <div className="bg-blue-500 text-white p-4">
       <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
       <p className="mt-2">This is a Vite + React.js app with Tailwind CSS.</p>
+      <button className="rounded-full ...">Save Changes</button>
+      <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
+             Click Me
+      </button>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.main.jsx"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
